2db2a024fe45ca8e78ec8a134631878374ae58ff
Pirateboyfoxy <146957835+Pirateboyfoxy@users.noreply.github.com>
2024-04-16 09:06:34 -0400

    Point Trigger

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BirdController : MonoBehaviour
 {
     Rigidbody2D bird;
+
+    int score = 0;
+
+    public Text scoreUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +23,17 @@ public class BirdController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             bird.AddForce(new Vector2(0,1) * 150);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Points"))
+        {
+            //increase score by 1
+            score = score + 1;
+
+            scoreUI.text = score.ToString();
         }
     }
 }
